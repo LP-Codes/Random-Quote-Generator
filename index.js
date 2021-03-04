83f6269b0d4242a45b75efa6d2fc608500eab32a
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-04 11:38:59 +0530

    Replace axios with fetch

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-url = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
-
+url = "https://type.fit/api/quotes";
 
 function hndlbtnclick() {
   console.log("btnclkd");
-  // fetch(url)
-  //   .then((res) => res.json())
-  //   .then(function (data) {
-  //     var quotes = document.getElementById("p");
-  //     var author = document.getElementById("h4");
-  //     author.innerHTML = "Author - " + data.author;
-  //     quotes.innerHTML = data.quote;
-  //   });
-
-  axios
-    .get(url)
-    .then(function (res) {
-      console.log(res);
+  fetch(url)
+    .then((res) => res.json())
+    .then(function (data) {
+      // get random data between 0-100
+      var x = Math.floor(Math.random() * 100 + 1);
+      // console.log(data[x].author);
+      // console.log(data[0]);
       var author = document.getElementById("h4");
       var quotes = document.getElementById("p");
-      author.innerHTML = "Author - " + res.data.author;
-      quotes.innerHTML = res.data.message;
-    })
-    .catch((err) => console.log(err));
+      author.innerHTML = "Author - " + data[x].author;
+      quotes.innerHTML = data[x].text;
+    });
+  // axios
+  //   .get(url)
+  //   .then(function (res) {
+  //     console.log(res);
+  //     var author = document.getElementById("h4");
+  //     var quotes = document.getElementById("p");
+  //     author.innerHTML = "Author - " + res.data.author;
+  //     quotes.innerHTML = res.data.message;
+  //   })
+  //   .catch((err) => console.log(err));
 }
 
 document.getElementById("btn").addEventListener("click", hndlbtnclick);
