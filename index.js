505149fdfbdaa57eba3f6d660fc309dfb8c318ba
LP-Codes <lovelesh_p@yahoomail.com>
2021-03-04 10:09:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+url = "http://quotes.stormconsultancy.co.uk/random.json";
+
+function hndlbtnclick() {
+  // fetch(url)
+  //   .then((res) => res.json())
+  //   .then(function (data) {
+  //     var quotes = document.getElementById("p");
+  //     var author = document.getElementById("h4");
+  //     author.innerHTML = "Author - " + data.author;
+  //     quotes.innerHTML = data.quote;
+  //   });
+
+  axios
+    .get(url)
+    .then(function (res) {
+      // console.log(res);
+      var author = document.getElementById("h4");
+      var quotes = document.getElementById("p");
+      author.innerHTML = "Author - " + res.data.author;
+      quotes.innerHTML = res.data.quote;
+    })
+    .catch((err) => console.log(err));
+}
+
+document.getElementById("btn").addEventListener("click", hndlbtnclick);
